3ee935e9e401add67f80fe802dfa62aaad8acad4
Abhijeet Kumar <abhijeetkumar111000@gmail.com>
2026-06-17 22:47:25 +0530

    Done

---
+++ 796. Rotate String/a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <map>
+#include <queue>      // For priority_queue (often used as a heap)
+#include <algorithm>  // For make_heap, sort, min, max, etc.
+
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        string doubled = s + s;
+        return doubled.contains(goal);
+    }
+};
+
+int main() {
+    // Optimize standard I/O operations for speed
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
