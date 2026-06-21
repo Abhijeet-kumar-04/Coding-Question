a9686edc8db012ad5d26ea163e6253a8771a13c4
Abhijeet Kumar <abhijeetkumar111000@gmail.com>
2026-06-21 22:53:22 +0530

    Done

---
+++ 1431. Kids With the Greatest Number of Candies/a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <map>
+#include <queue>      // For priority_queue (often used as a heap)
+#include <algorithm>  // For make_heap, sort, min, max, etc.
+
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int mx = *max_element(candies.begin(), candies.end());
+
+        vector<bool> ans;
+        for (int x : candies) {
+            ans.push_back(x + extraCandies >= mx);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    // Optimize standard I/O operations for speed
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
