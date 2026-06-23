c5ee59a450abe17eadff582495cf4c8ff1b97f62
Abhijeet Kumar <abhijeetkumar111000@gmail.com>
2026-06-23 23:16:16 +0530

    Done

---
+++ 2300. Successful Pairs of Spells and Potions/a.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+template<typename T>
+T gcd(T a, T b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+    return a / gcd(a, b) * b;
+}
+
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size();
+        vector<int> ans(n) ;
+        sort(potions.begin(),potions.end());
+        for(int i =0;i<n;i++){
+            long long remaining = success/spells[i] ;
+            int pos;
+            if(remaining* spells[i] == success ) {
+                pos = lower_bound(potions.begin(),potions.end(),remaining) - potions.begin();
+            }
+            else{
+                pos = upper_bound(potions.begin(),potions.end(),remaining) - potions.begin();
+
+            }
+            ans[i]=potions.size() - pos;
+        }
+        return ans ;
+    }
+};
+
+int main() {
+    fastio();
+
+    
+
+    return 0;
+}
+
