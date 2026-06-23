a72b0ad9cbf780c40abe397f85e426dc23d8a225
Abhijeet Kumar <abhijeetkumar111000@gmail.com>
2026-06-24 00:25:25 +0530

    Done

---
+++ 649. Dota2 Senate/a.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+template<typename T>
+T gcd(T a, T b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+    return a / gcd(a, b) * b;
+}
+
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n = senate.size();
+        queue<int> radiant, dire;
+        for (int i = 0; i < n; i++) {
+            if (senate[i] == 'R') radiant.push(i);
+            else dire.push(i);
+        }
+        while (!radiant.empty() && !dire.empty()) {
+            int r = radiant.front();
+            int d = dire.front();
+            radiant.pop();
+            dire.pop();
+            if (r < d) {
+                radiant.push(r + n);
+            } else {
+                dire.push(d + n);
+            }
+        }
+        return radiant.empty() ? "Dire" : "Radiant";
+    }
+};
+
+int main() {
+    fastio();
+
+    
+
+    return 0;
+}
+
