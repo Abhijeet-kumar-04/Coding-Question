747f1313a28334720cc5ed0bf29814b64895784e
Abhijeet Kumar <abhijeetkumar111000@gmail.com>
2026-06-24 00:17:32 +0530

    Done

---
+++ 933. Number of Recent Calls/a.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define ff first
+#define ss second
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+template<typename T>
+T gcd(T a, T b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+    return a / gcd(a, b) * b;
+}
+
+class RecentCounter {
+public:
+    queue<int> q;
+
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+
+        while (!q.empty() && q.front() < t - 3000) {
+            q.pop();
+        }
+
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+
+int main() {
+    fastio();
+
+    
+
+    return 0;
+}
+
